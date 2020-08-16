0ec8bf01076b2645b0197eb70c9b7969e3d9fb34
Riley Slayden <slaydenriley@gmail.com>
2020-08-16 16:03:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* Your Code Here */
-
+function createEmployeeRecord() {
+  
+};
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
